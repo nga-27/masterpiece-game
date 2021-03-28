7c1061449565385184bbccc423e3c9c923bb9eb9
a394szz <ngamell@mmm.com>
2021-03-28 14:32:37 -0500

    github image storage test complete

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import os
+import io
 import json
 import secrets
+import requests
+
+from PIL import Image
 
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
@@ -10,12 +14,16 @@ from app.dependencies import metadata_tags
 DB_DIR = os.path.join("app", "db")
 DB_PATH = os.path.join(DB_DIR, "db.json")
 
+RESOURCE_PATH = os.path.join(DB_DIR, "resources.json")
+RESOURCE_URL = "https://raw.githubusercontent.com/nga-27/masterpiece-game-resources/main/content.json"
+
 app = FastAPI(
     title="Masterpiece Game API Server",
     description="The FastAPI version of the 60s-70s game 'Masterpiece'.",
     version="0.0.1",
     openapi_tags=metadata_tags.tags_metadata
 )
+
 
 def init_db():
     if not os.path.exists(DB_DIR):
@@ -30,8 +38,46 @@ def init_db():
     return db
 
 
+def init_resources():
+    if not os.path.exists(DB_DIR):
+        os.mkdir(DB_DIR)
+    if os.path.exists(RESOURCE_PATH):
+        os.remove(RESOURCE_PATH)
+
+    res = requests.get(RESOURCE_URL)
+    with open(RESOURCE_PATH, 'wb') as res_file:
+        res_file.write(res.content)
+        res_file.close()
+
+    with open(RESOURCE_PATH, 'r') as resf:
+        db = json.load(resf)
+        resf.close()
+
+    return db
+
+
 DB = init_db()
+RESOURCES = init_resources()
+
 
 @app.get("/", tags=["Health"])
 def check_heartbeat():
     return {"hello there": "from Masterpiece"}
+
+
+@app.get("/fetch_paintings", tags=["Health"])
+def fetch_paintings():
+    return RESOURCES
+
+
+@app.get("/dl", tags=["Health"])
+def download():
+    # print(RESOURCES)
+    painting = RESOURCES['art'][list(RESOURCES['art'].keys())[0]]
+    url = os.path.join(RESOURCES['config']['base_url'], painting['filename'])
+    res = requests.get(url)
+    link_path = os.path.join('app', 'db', painting['filename'])
+    with Image.open(io.BytesIO(res.content)) as paintf:
+        paintf.save(link_path)
+
+    return {"status": "ok"}
